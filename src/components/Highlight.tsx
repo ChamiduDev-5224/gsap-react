62cfd4e63a58949f45f2d035c65c707268742d7b
Rachitha95 <rachitharangana9522@gmail.com>
2024-03-20 16:25:53 +0530

    highlight header animate

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -30,17 +30,24 @@ export const Highlight = () => {
             y: -50,
             opacity: 1
         })
+        gsap.to('#h_title', {
+            delay: 1,
+            y: 20,
+            opacity: 1
+        })
+
+
     }, [])
     return (
-        <section className='min-h-[100vh] mt-4 md:mt-16'>
+        <section className='min-h-[100vh] mt-4 md:mt-16 bg-black'>
             <div>
-                <h2 className='text-default text-5xl font-semibold my-4 text-center'>iPhone 15 Pro</h2>
+                <h2 className='text-default text-5xl font-semibold mb-4 text-center opacity-0' id='h_title'>iPhone 15 Pro</h2>
                 <video autoPlay muted playsInline={true} key={videoSrc}>
                     <source src={videoSrc} type='video/mp4' />
                 </video>
             </div>
 
-            <div id='get_start' className='flex flex-col items-center opacity-0'>
+            <div id='get_start' className='flex flex-col items-center opacity-0 mt-5 md:mt-0'>
                 <a href="#" className='bg-stone-800 text-yellow-50 py-4 rounded-xl px-4 w-fit shadow-lg hover:shadow-gray-600'>Get Offer</a>
                 <span className='text-default my-5 font-bold'>Get Offer For 99$</span>
             </div>
